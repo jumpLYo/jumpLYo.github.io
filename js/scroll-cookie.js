18d01766cc464ce2dc23633cc8d671d2c246f796
dcr <330296190@qq.com>
2019-06-01 01:30:05 +0800

    Site updated: 2019-06-01 01:30:05

--- js/scroll-cookie.js
+++ js/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Jun 01 2019 01:20:59 GMT+0800 (CST)
+// build time:Sat Jun 01 2019 01:22:31 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
