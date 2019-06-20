7bc6adfff6ac6c7c3e9c875775b93b3fbee47216
dcr <330296190@qq.com>
2019-06-20 18:48:39 +0800

    Site updated: 2019-06-20 18:48:39

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Wed Jun 19 2019 17:22:25 GMT+0800 (CST)
+// build time:Thu Jun 20 2019 18:48:29 GMT+0800 (China Standard Time)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
